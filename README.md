6eefae13230a7b9c70561fed928dc00a78dc7d11
dagrende <dag.rende@find-out.se>
2016-05-22 13:02:57 +0200

    now works with triple graphs instead of json-ld objects

--- README.md
+++ README.md
@@ -1,2 +1,29 @@
 # oslc-schema-viewer
-Extracts schema information from an OSLC endpoint and draws a Domain Specification View
+Extracts schema information from an OSLC provider and draws a Domain Specification View
+
+## Overview
+Starting with an OSLC catalog URL, this web application
+- loads the catalog resource and uses it to find all resource types, domains and resource shapes
+- draws a diagram of all domains, resource types and properties.
+
+![Domain Specification View example](docs/domainSpecificationView.png)
+
+
+## OSLC Meta model
+
+Parts relevant for this application.
+
+![meta model](http://yuml.me/4acb73c3)
+
+## Metadata Retrieval Algorithm
+
+- get the catalog
+    - iterate over each service provider in it
+        - for each service
+            - for each QueryCapability and CreationFactory
+                - get the resourceShape - save it
+                    - for each property in it
+                        - find out resource type by type of any found resource
+                        - find out reference property target type by
+                          1. oslc:range
+                          1. type of target resource in instance
--- app/js/main.js
+++ app/js/main.js
@@ -3,57 +3,57 @@ var _ = require('lodash');
 var Promise = require('promise');
 var jsonld = require('jsonld');
 var RdfXmlParser = require('rdf-parser-rdfxml');
+var $rdf = require('rdflib');
 
 var parser = new RdfXmlParser();
 var catalogUrl = 'https://vservices.offis.de/rtp/simulink/v1.0/services/catalog/singleton';
 var resultMap = {}; // {<sp>: {<resource>: {<prop>: {name: xxx, ...}, ...}, ...}}
-fetchIndexedJsonLd(catalogUrl).then(function(indexedCatalog) {
-  // got a service provider catalog - find all service providers in it
-  // console.log(catalogUrl, indexedCatalog);
-  var serviceProviderUrls = indexedCatalog[catalogUrl]['http://open-services.net/ns/core#serviceProvider'];
-  // promise with first resourceShape for queryCapability for each serviceProvider in indexedCatalog
-  return Promise.all(_.map(serviceProviderUrls, function(serviceProviderUrlObj) {
-    var serviceProviderUrl = serviceProviderUrlObj['@id'];
-    // console.log('serviceProviderUrl', serviceProviderUrl);
-    return fetchIndexedJsonLd(serviceProviderUrl).then(function(indexedServiceProvider) {
-      // got a serviceProvider - find all services in it
-      // console.log(serviceProviderUrl, indexedServiceProvider);
-      return Promise.all(_.map(indexedServiceProvider[serviceProviderUrl]['http://open-services.net/ns/core#service'], function(serviceObj) {
-        // got a service - find resource shape in it
-        var serviceId = serviceObj['@id'];
-        var creationFactoryId = indexedServiceProvider[serviceId]['http://open-services.net/ns/core#queryCapability'][0]['@id'];
-        var resourceShapeUrl = indexedServiceProvider[creationFactoryId]['http://open-services.net/ns/core#resourceShape'][0]['@id'];
-        // console.log('  resourceShapeUrl', resourceShapeUrl);
-        return fetchIndexedJsonLd(resourceShapeUrl).then(function(indexedResourceShape) {
-          // got resourceShape - traverse and put props into index
-          return addResourcePropsToMap(serviceProviderUrl, resourceShapeUrl, indexedResourceShape);
-        });
-      }));
-    });
-  })).then(function(resultList) {
-    // get here when all reource shapes are loaded and their props indexed into resultMap
-    // console.log('resultMap', resultMap);
-    return resultMap;
-  });
-})
-.then(renderHtmlPropsTable)
-.catch(function (error) {
-  console.error(error);
-  console.error(error.stack);
-})
-;
 
-function fetchIndexedJsonLd(url) {
-  // console.log('1/4 fetchIndexedJsonLd(',url,')');
+console.log(parser.rdf.createNamedNode());
+
+// fetch catalog
+fetchGraph(catalogUrl).then(function(graph) {
+  let resourceShapeUriSet = {};
+  let serviceProvidersTriples = graph.match(null, 'http://www.w3.org/1999/02/22-rdf-syntax-ns#type', 'http://open-services.net/ns/core#ServiceProvider');
+  for (let serviceProviderUriTriple of serviceProvidersTriples.toArray()) {
+    console.log(serviceProviderUriTriple.subject.toString());
+    let serviceTriples = graph.match(serviceProviderUriTriple.subject, 'http://open-services.net/ns/core#service', null);
+    for (let serviceTriple of serviceTriples.toArray()) {
+      let queryCapabilityTriples = graph.match(serviceTriple.object, 'http://open-services.net/ns/core#queryCapability', null);
+      for (let queryCapabilityTriple of queryCapabilityTriples.toArray()) {
+        let resourceShapeUriTriples = graph.match(queryCapabilityTriple.object, 'http://open-services.net/ns/core#resourceShape', null);
+        for (let resourceShapeUriTriple of resourceShapeUriTriples.toArray()) {
+          resourceShapeUriSet[resourceShapeUriTriple.object.toString()] = 'dummy';
+        }
+      }
+    }
+  }
+
+  // fetch all resourceShape resources
+  Promise.all(_.map(resourceShapeUriSet, function(value, resourceShapeUri) {
+    return fetchGraph(resourceShapeUri).then(function(resourceShapeGraph) {
+      // add resourceShape triples to total graph
+      graph.addAll(resourceShapeGraph);
+      return resourceShapeUri;
+    })
+  })).done(function(resourceShapeUris) {
+    // all shapes are loaded - log all property names
+    for (let resourceShapeUri of resourceShapeUris) {
+      console.log(resourceShapeUri);
+      graph.match(resourceShapeUri, 'http://open-services.net/ns/core#property', null).forEach(function(propRefTriple) {
+        graph.match(propRefTriple.object, 'http://open-services.net/ns/core#name', null).forEach(function(propNameTriple) {
+          console.log(' ', propNameTriple.object.toString());
+        });
+      });
+    }
+  })
+}).catch(function(err) {
+  console.error(err);
+});
+
+function fetchGraph(url, tripleMap) {
   return fetchXml(url).then(function(urlData) {
-    // console.log('2/4 fetchIndexedJsonLd dom:', urlData);
     return parser.parse(urlData);
-  }).then(function (graph) {
-    // console.log('3/4 fetchIndexedJsonLd graph', graph);
-    return jsonld.promises.fromRDF(graphToString(graph), {format: 'application/nquads'});
-  }).then(function (urlDataJsonLd) {
-    // console.log('4/4 fetchIndexedJsonLd jsonLd', urlDataJsonLd);
-    return _.keyBy(urlDataJsonLd, '@id');
   });
 }
 
@@ -68,59 +68,6 @@ function fetchXml(url) {
         // console.log('fetchXml document', doc);
         fulfill(doc);
       }
-    });
-  });
-}
-
-function addResourcePropsToMap(serviceProviderUrl, resourceShapeUrl, indexedResourceShape) {
-  // console.log('addResourcePropsToMap', serviceProviderUrl, resourceShapeUrl, indexedResourceShape);
-  var shape = _.find(indexedResourceShape, (value, key)=>value['@type']=='http://open-services.net/ns/core#ResourceShape');
-  var resourceType = shape['http://open-services.net/ns/core#describes'][0]['@id'];
-  // console.log('resourceType', resourceType);
-  if (!resultMap[serviceProviderUrl]) {
-    resultMap[serviceProviderUrl] = {};
-  }
-  resultMap[serviceProviderUrl][resourceType] = _.map(indexedResourceShape[resourceShapeUrl]['http://open-services.net/ns/core#property'], function(propertyId) {
-    var property = indexedResourceShape[propertyId['@id']];
-    function getPropertyProperty(id, selector) {
-      var prop = property['http://open-services.net/ns/core#' + id];
-      return prop && prop[0][selector]
-        .replace('http://open-services.net/ns/core#', 'oslc:')
-        .replace('http://open-services.net/ns/cm#', 'cm:')
-        .replace('http://purl.org/dc/terms/', 'purl:')
-        .replace('http://www.w3.org/2001/XMLSchema#', 'xsd:')
-        .replace('http://www.w3.org/1999/02/22-rdf-syntax-ns#', 'rdf:')
-    }
-    return {
-      id: propertyId['@id'],
-      name: getPropertyProperty('name', '@value'),
-      propertyDefinition: getPropertyProperty('propertyDefinition', '@id'),
-      occurs: getPropertyProperty('occurs', '@id'),
-      readOnly: getPropertyProperty('readOnly', '@value'),
-      valueType: getPropertyProperty('valueType', '@id')
-    };
-  });
-  return resultMap;
-}
-
-function renderHtmlPropsTable(resultMap) {
-  // console.log('renderHtmlPropsTable(', resultMap, ')');
-  _.forEach(resultMap, function(spResourceProps, serviceProviderUrl) {
-    // console.log('renderHtmlPropsTable key', serviceProviderUrl);
-    d3.select('#graph').append('h2').text(serviceProviderUrl);
-    _.forEach(spResourceProps, function(props, id) {
-      d3.select('#graph').append('h3').text(id);
-      var table = d3.select('#graph').append('table');
-      table.append('tr').attr('class', 'thead');
-      var keys = ['name', 'valueType', 'occurs', 'readOnly'];
-      table.select('tr').selectAll('td')
-        .data(keys).enter().append('td').attr('class', 'thead').text(d=>d);
-      table.selectAll('.proprow')
-        .data(props, d=>d.id)
-        .enter().append('tr')
-        .attr('class', '.proprow')
-        .selectAll('td')
-        .data(d=>_.map(keys, key=>d[key])).enter().append('td').text(d=>d);
     });
   });
 }
